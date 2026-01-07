a97e108ddbbe114430ef72a1f5fd568fc56437ea
kfiddle <kenjfiddle@gmail.com>
2026-01-07 14:32:54 -0600

    first

---
+++ public/src/components/header/Header.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+const Header = () => {
+    const handleKenClick = () => {
+        console.log("Ken");
+    };
+
+    const handleJulieClick = () => {
+        console.log("Julie");
+    };
+
+    return (
+        <header className={styles.header}>
+            <div className={styles.logoDiv}>
+                <h1 className={styles.logo}>
+                    <span className={styles.highlight}>J</span>ulie
+                    <span className={styles.highlight}>K</span>en
+                </h1>
+            </div>
+
+            <div className={styles.navContainer}>
+                <nav className={styles.nav}>
+                    <ul>
+                        <li className={styles.navItem}>
+                            <button type="button" className={styles.inactive} onClick={handleKenClick}>
+                                <span className={styles.label}>Julie</span>
+                            </button>
+                        </li>
+                        <li className={styles.navItem}>
+                            <button type="button" className={styles.inactive} onClick={handleJulieClick}>
+                                <span className={styles.label}>Ken</span>
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+
+                {/* Right side user area removed for now */}
+            </div>
+        </header>
+    );
+};
+
+export default Header;
